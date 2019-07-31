41170f06a5de37fe6ec7814cfd86369f6abb0c85
EC2 Default User <ec2-user@ip-172-31-40-227.us-east-2.compute.internal>
2019-07-31 02:12:22 +0000

    some sort of comment

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,16 @@
 import os
 from app import app
-from flask import render_template, request, redirect
-
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
-
-
-# from flask_pymongo import PyMongo
+from flask import render_template, request, redirect, session, url_for
+from flask_pymongo import PyMongo
+app.secret_key = b'.\xe2l\xb1\x0cg\xa8:\x0e(N\x1c\xd4\x8fPZ'
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name' 
+app.config['MONGO_DBNAME'] = 'database' 
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri' 
+app.config['MONGO_URI'] = 'mongodb+srv://admin:4wxbgw55@cluster0-reiqc.mongodb.net/database?retryWrites=true&w=majority' 
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 
 # INDEX
@@ -27,7 +20,22 @@ events = [
 
 def index():
     return render_template('index.html')
+@app.route('/login', methods=["POST"])
 
+def login():
+    users = mongo.db.users
+    #use the username to find account, then verify password
+    existing_user = users.find_one({'username':request.form['username']})
+    if existing_user:
+        #verify password
+        if existing_user['password'] == request.form['password']:
+            session['username'] = request.form['username']
+            return render_template('/index.html')
+        else:
+            return "Invalid password"
+        return "Invalid username"
+    else:
+        return "Enter a valid username/password"
 
 # CONNECT TO DB, ADD DATA
 
@@ -40,3 +48,23 @@ def add():
 
     # return a message to the user
     return ""
+    
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method =="POST":
+        # take in the info they gave us, check if username is taken, if username is available put into a database of users
+        users=mongo.db.users
+        existing_user = users.find_one({'username':request.form['username']})
+        if existing_user is None:
+            users.insert({"username":request.form['username'],'password':request.form["password"]})
+            return redirect('/')
+        else:
+            return "That username is taken."
+            
+    else:
+        return render_template('signup.html')
+@app.route('/logout') 
+def logout():
+    session.clear()
+    return redirect('/')
+
